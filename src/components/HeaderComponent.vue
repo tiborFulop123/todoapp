7c83d8073957135230f8675bcf679d0c7ef7cbab
Tibor_Fulop <fuloptibor180@gmail.com>
2023-02-01 11:30:14 +0200

    ITL-7 Create the header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,29 +1,72 @@
 <template>
-    <header>
-      <h1>Header </h1>
-    </header>
-  </template>
+  <header>
+
+
+    <div class="flex items-center">
+      <span class="inline-block align-middle TodoHeader">
+        To Do List
+      </span><input type="text" v-model="todoText" />
+      <button  @click="addToDo(todoText)"><img class=" h-[59.64px;] w-[59.64px]" :src="pluszGomb" ></button>
+      
+    </div>
+    <div>
+
+
+    </div>
+  </header>
+  <!-- 
+
+    <div>
+      <input type="text" v-model="todoText" />
+      <button class="" @click="addToDo(todoText)">&#43;</button> 
+    </div>
+   -->
+  <!-- 
+   <TodoForm     @toDoAdded="addNewTodo">   </TodoForm> -->
+
+
+
+   
+</template>
+
+<script setup >
+import TodoForm from "./TodoForm.vue";
+import pluszGomb from "./../assets/pluszGomb.svg"
+import { ref } from "vue";
+
+const todoText = ref("");
+const emit = defineEmits(["toDoAdded"]);
+function addToDo() {
+  emit("toDoAdded", todoText.value);
+  todoText.value = "";
+}
+
+// export default {
+//   data() {
+//     return {
+//       title: "To Do List",
+//     };
+//   },
+//   components: { TodoForm },
+// };
+
+// const toDos = ref([]);
+// function addNewTodo(todo) {
+//   toDos.value.push(todo);
+// }
+// // const emit = defineEmits(["toDoAdded"])
+</script>
+
+<style>
+.TodoHeader {
   
-  <script>
-  export default {
-    data() {
-      return {
-        title: 'Header'
-      }
-    }
-  }
-  </script>
-  
-  <style>
-  header {
-    background-color: #333;
-    color: rgb(155, 29, 29);
-    padding: 20px;
-    text-align: center;
-  }
-  
-  h1 {
-    margin: 12;
-  }
-  </style>
-  
+  color: #000000;
+
+  font-family: "Neue Haas Grotesk Display Pro";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 72px;
+  line-height: 86px;
+}
+
+</style>
